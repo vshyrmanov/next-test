22d9a526da2abc44dec2c688124bc10964760156
vshyrmanov <shyrmanovvm@gmail.com>
2023-01-09 19:53:02 +0200

    add head tags

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,9 @@ export default function App({ Component, pageProps }) {
         <title>Гидрошпонки деформационных и рабочих швов</title>
         <meta name="keywords" content="Уплотнители, Уплотнители киев, Уплотнители Украина, резинотехнические шнуры ТМКЩ, трубки МБС, соэкструзия" />
         <meta name="description" content="Гидрошпонки деформационных и рабочих швов, Уплотнители, резинотехнические шнуры ТМКЩ, трубки МБС, соэкструзия"/>
-        {/*<base href="https://next-test-54ch.vercel.app/" />*/}
+        <base href="https://next-test-54ch.vercel.app/" />
         <meta property="og:type" content="website" />
-        {/*<meta property="og:url" content="https://next-test-54ch.vercel.app/" />*/}
+        <meta property="og:url" content="https://next-test-54ch.vercel.app/" />
         <meta property="og:site_name" content="Гидрошпонки деформационных и рабочих швов" />
         <meta property="og:title" content="Гидрошпонки деформационных и рабочих швов" />
 
