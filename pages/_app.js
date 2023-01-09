5e4fcd9745bd50d97513cf6237aba8b001b427d0
vshyrmanov <shyrmanovvm@gmail.com>
2023-01-09 19:33:37 +0200

    add head tags

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,13 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Гидрошпонки деформационных и рабочих швов</title>
-        <meta
-          name="description"
-          content="Уплотнители, резинотехнические шнуры ТМКЩ, трубки МБС, соэкструзия"
-        />
-        <meta name="keywords" content="Уплотнители, резинотехнические шнуры ТМКЩ, трубки МБС, соэкструзия"/>
-        <meta name="description" content="Уплотнители, резинотехнические шнуры ТМКЩ, трубки МБС, соэкструзия"/>
+        <meta name="keywords" content="Уплотнители, Уплотнители киев, Уплотнители Украина, резинотехнические шнуры ТМКЩ, трубки МБС, соэкструзия" />
+        <meta name="description" content="Гидрошпонки деформационных и рабочих швов, Уплотнители, резинотехнические шнуры ТМКЩ, трубки МБС, соэкструзия"/>
+        {/*<base href="https://next-test-54ch.vercel.app/" />*/}
+        <meta property="og:type" content="website" />
+        {/*<meta property="og:url" content="https://next-test-54ch.vercel.app/" />*/}
+        <meta property="og:site_name" content="Гидрошпонки деформационных и рабочих швов" />
+        <meta property="og:title" content="Гидрошпонки деформационных и рабочих швов" />
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/*<link rel="icon" href="/favicon.ico" />*/}
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,16 +1,33 @@
 import React from 'react';
 import {data} from "../../data/data";
 import { useRouter } from 'next/router'
+import Head from "next/head";
 
 const ProductPage = () => {
 	const {query} = useRouter();
 
 	return (
-		<div>
-			{query.id ? data[query.id].map((e, index) =>
-				<React.Fragment key={index}>{e}</React.Fragment>
-			) : <h2>Loading ...</h2>}
-		</div>
+		<>
+			<Head>
+				<title>Техпластины МБС и техпластины ТМКЩ</title>
+				<meta name="keywords" content="Уплотнители, Уплотнители киев, Уплотнители Украина, резинотехнические шнуры ТМКЩ, трубки МБС, соэкструзия, Техпластины предназначена для изготовления уплотнительных деталей в трансформаторных и электротехнических устройствах." />
+				<meta name="description" content="Техпластины предназначена для изготовления уплотнительных деталей в трансформаторных и электротехнических устройствах."/>
+				{/*<base href="https://next-test-54ch.vercel.app/products/gvr" />*/}
+				<meta property="og:type" content="website" />
+				{/*<meta property="og:url" content="https://next-test-54ch.vercel.app/products/gvr" />*/}
+				<meta property="og:site_name" content="Техпластины МБС и техпластины ТМКЩ" />
+				<meta property="og:title" content="Техпластины МБС и техпластины ТМКЩ" />
+
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				{/*<link rel="icon" href="/favicon.ico" />*/}
+			</Head>
+			<div>
+				{query.id ? data[query.id].map((e, index) =>
+					<React.Fragment key={index}>{e}</React.Fragment>
+				) : <h2>Loading ...</h2>}
+			</div>
+		</>
+
 	);
 };
 
