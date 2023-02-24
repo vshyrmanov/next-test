05eb30a53617038aeea5672ab6e456141570c775
vshyrmanov <shyrmanovvm@gmail.com>
2023-02-24 22:12:08 +0200

    add responsive design

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from "next/image";
 import style from '../../../styles/general.module.scss';
@@ -10,15 +10,16 @@ import {img} from "../../../assets";
 import {useRouter} from "next/router";
 
 const Header = () => {
-	const [open, setOpen] = useState(false);
+	const [isMenu, setIsMenu] = useState(false);
 	const {query, pathname} = useRouter();
+	const menuHandler = () => setIsMenu(prev => !prev);
 
 	return (
 		<>
 			<header className={style.header}>
 				<nav>
 					<div className={style.burger}>
-						<RxHamburgerMenu onClick={() => setOpen(prev => !prev)} />
+						<RxHamburgerMenu onClick={menuHandler} />
 					</div>
 					<div className={style.title}>
 						<Link href='/' className={style.link}>
@@ -26,49 +27,50 @@ const Header = () => {
 						</Link>
 					</div>
 					<div className={style.menu}>
-						<Link href='/manufacturing' className={style.link}>
+						<div className={style.dropdownBtn}>
+							<button onClick={menuHandler}>Продукция</button>
+							<div className={!isMenu ? cn(style.dropdown_menu, styles.disable) : style.dropdown_menu}>
+									{links.map(e =>
+										<Link key={e.link} href={`/products/${e.link}`} className={styles.link}>
+											<button
+												className={query?.id === e.link ? cn(styles.btnLink, styles.activeLink) : styles.btnLink}
+												onClick={menuHandler}
+											>{e.label}</button>
+										</Link>
+									)}
+									<div className={style.divider} />
+									<Link href='/manufacturing' className={cn(style.link, style.response_navLink)}>
+										<button
+											onClick={menuHandler}
+											className={pathname === "/manufacturing" ? cn(styles.btnLink, styles.activeLink) : styles.btnLink}
+										>Производство гидрошпонки</button>
+									</Link>
+									<Link href='/delivery' className={cn(style.link, style.response_navLink)}>
+										<button
+											onClick={menuHandler}
+											className={pathname === "/delivery" ? cn(styles.btnLink, styles.activeLink) : styles.btnLink}
+										>Доставка</button>
+									</Link>
+									<Link href='/contacts' className={cn(style.link, style.response_navLink)}>
+										<button
+											onClick={menuHandler}
+											className={pathname === "/contacts" ? cn(styles.btnLink, styles.activeLink) : styles.btnLink}
+										>Контакты</button>
+									</Link>
+							</div>
+						</div>
+						<Link href='/manufacturing' className={cn(style.link, style.menu_link)}>
 							<button className={pathname === "/manufacturing" ? styles.activeNavLink : ""}>Производство гидрошпонки</button>
 						</Link>
-						<Link href='/delivery' className={style.link}>
+						<Link href='/delivery' className={cn(style.link, style.menu_link)}>
 							<button className={pathname === "/delivery" ? styles.activeNavLink : ""}>Доставка</button>
 						</Link>
-						<Link href='/contacts' className={style.link}>
+						<Link href='/contacts' className={cn(style.link, style.menu_link)}>
 							<button className={pathname === "/contacts" ? styles.activeNavLink : ""}>Контакты</button>
 						</Link>
 					</div>
 				</nav>
 			</header>
-			<div className={open ? cn(styles.sidebar, styles.sidebarOpen) : styles.sidebar}>
-				<nav className={styles.sidebarNav}>
-								{links.map(e =>
-									<Link key={e.link} href={`/products/${e.link}`} className={styles.link}>
-										<button
-											className={query?.id === e.link ? cn(styles.btnLink, styles.activeLink) : styles.btnLink}
-											onClick={() => setOpen(false)}
-										>{e.label}</button>
-									</Link>
-								)}
-					<div className={style.divider} />
-					<Link href='/manufacturing' className={cn(style.link, style.response_navLink)}>
-						<button
-							onClick={() => setOpen(false)}
-							className={pathname === "/manufacturing" ? cn(styles.btnLink, styles.activeLink) : styles.btnLink}
-						>Производство гидрошпонки</button>
-					</Link>
-					<Link href='/delivery' className={cn(style.link, style.response_navLink)}>
-						<button
-							onClick={() => setOpen(false)}
-							className={pathname === "/delivery" ? cn(styles.btnLink, styles.activeLink) : styles.btnLink}
-						>Доставка</button>
-					</Link>
-					<Link href='/contacts' className={cn(style.link, style.response_navLink)}>
-						<button
-							onClick={() => setOpen(false)}
-							className={pathname === "/contacts" ? cn(styles.btnLink, styles.activeLink) : styles.btnLink}
-						>Контакты</button>
-					</Link>
-				</nav>
-			</div>
 		</>
 
 	);
